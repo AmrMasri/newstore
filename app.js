9f0f452162db32e6c98f59ba1b87ff4b0d90edd2
amr <amrmasri999@gmail.com>
2024-02-26 18:56:35 +0300

    i started add to cart but there is a problem i finsh all the video i have only 6 minut to compelte th video

---
+++ app.js
@@ -0,0 +1,117 @@
+let iconCart = document.querySelector('.icon-cart');
+let closeCart = document.querySelector('.close');
+let body = document.querySelector('body');
+let listProductHTML = document.querySelector('.pro-container')
+let listCartHTML = document.querySelector('.listCart');
+let iconCartSpan = document.querySelector('.icon-cart span')
+
+    let listProducts = [];
+    let carts = [];
+
+iconCart.addEventListener('click', () =>{
+    body.classList.toggle('showCart')
+})
+closeCart.addEventListener('click', () => {
+    body.classList.toggle('showCart')
+})
+
+const addDataToHTML = () => {
+    listProductHTML.innerHTML = '';
+    if(listProducts.length > 0 ){
+        listProducts.forEach(product => {
+            let newProduct = document.createElement('div');
+            newProduct.classList.add('item');
+            newProduct.dataset.id = product.id;
+            newProduct.innerHTML = `
+        <div class="pro-container" class= "section-p1">
+            <div class="pro" >
+                <img src=${product.image} alt="">
+                <div class="des">
+                    <span>${product.name}</span>
+                    <h5>Cartoon Astronaut T-Shirts</h5>
+                    <div class="stars">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                    </div>
+                    <h4>${product.price}</h4>
+                </div>
+            <button class="addCart">
+                Add To Cart
+                </button>
+                </div>
+            `;
+            listProductHTML.appendChild(newProduct);
+        })
+    }
+}
+
+    listProductHTML.addEventListener('click', (event) =>{
+        let positionClick = event.target;
+        if(positionClick.classList.contains('addCart')){
+            let product_id = positionClick.parentElement.dataset.id;
+            addToCart(product_id)
+        }
+    })
+
+    const addToCart = (product_id) => {
+        let positionThisProductInCart = carts.findIndex((value) => value.product_id == product_id);
+        if(carts.length <= 0){
+            carts = [{
+                product_id: product_id,
+                quantity: 1
+            }]
+        }else if(positionThisProductInCart < 0){
+            carts.push({
+                product_id: product_id,
+                quantity: 1
+            });
+        }else{
+            carts[positionThisProductInCart].quantity = carts[positionThisProductInCart].quantity +1;
+            console.log(carts)
+        }
+        addCartToHTML();
+        }
+
+    const addCartToHTML = () => {
+        listCartHTML.innerHTML = '';
+        if(carts.length > 0){
+            carts.forEach(cart =>{
+                let newCart = document.createElement('div')
+                newCart.classList.add('item');
+                let positionProduct = listProducts.findIndex((value) => value.id == cart.product_id);
+                let info = listProducts [positionProduct];
+                newCart.innerHTML = `
+                <div class="image">
+                <img src=${info.image} alt="">
+            </div>
+                <div class="name">
+                    ${info.name}
+                </div>
+                <div class="totalPrice">
+                    ${info.price}
+                </div>
+                <div class="quantity">
+                    <span class="minus"><</span>
+                    <span>${cart.quantity}</span>
+                    <span class="plus">></span>
+                </div>
+                `
+                listCartHTML.appendChild(newCart);
+            })
+        }
+    }
+
+
+const initApp = () => {
+    //get data from json
+    fetch('products.json')
+    .then(response => response.json())
+    .then(data => {
+        listProducts = data;
+        addDataToHTML();
+    })
+}
+initApp();
